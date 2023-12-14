c2ad38b8079df51edbe9ae824849562403e68a3c
Daniel <daniel@Air-de-Daniel.local.tld>
2023-12-14 22:34:28 +0100

    j

--- p3/README.md
+++ p3/README.md
@@ -5,3 +5,42 @@ k3d cluster list
 k3d cluster start
 k3d cluster stop
 
+https://argo-cd.readthedocs.io/en/stable/getting_started/
+
+
+
+argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace default
+
+
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: argocd-wilapp
+  namespace: argocd
+spec:
+  destination:
+    namespace: dev
+    server: https://kubernetes.default.svc
+  project: default
+  source:
+    repoURL: https://github.com/zamazzal/iot-app.git
+    path: .
+    targetRevision: master
+  syncPolicy:
+    prune: true
+    selfHeal: true
+
+
+argocd app get guestbook  (view status of app)
+Name:               guestbook
+Server:             https://kubernetes.default.svc
+Namespace:          default
+URL:                https://10.97.164.88/applications/guestbook
+Repo:               https://github.com/argoproj/argocd-example-apps.git
+Target:
+Path:               guestbook
+Sync Policy:        <none>
+Sync Status:        OutOfSync from  (1ff8a67)
+Health Status:      Missing
+
+argocd app sync guestbook (sync manually, This command retrieves the manifests from the repository and performs a kubectl apply of the manifests.)
--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,17 +1,16 @@
+printf "\e[1;36m[K3S]🐳 : creating cluster K3d.\n\e[0m"
 sudo k3d cluster create p3
-#k3d cluster create zamazzalCluster -p "8888:30080"
-
 sudo kubectl cluster-info
-
+printf "\e[1;36m[K3S]🐳 : creating namespaces argodev y dev K3d.\n\e[0m"
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
 sudo kubectl get namespace
-printf "\e[1;33m[ARGOCD]🐙 : Installing K3S.\n\e \e[0m"
-echo "[ARGOCD] : install argocd and wait for pods to rollout..."
+printf "\e[1;33m[ARGOCD]🐙 : installing K3S.\n\e[0m"
+printf "\e[1;33m[ARGOCD]🐙 : install argocd and wait for pods to rollout...\n\e[0m"
 sudo kubectl apply -n argocd -f /p3/p3/confs/argocd_install.yaml
 sudo kubectl apply -n argocd -f /p3/p3/confs/argocd_ingress.yaml
 
-printf "\e[1;33m[ARGOCD]🐙 : wait for argocd pods to be running \e[0m"
+printf "\e[1;33m[ARGOCD]🐙 : wait for argocd pods to be running \n\e[0m"
 sudo kubectl rollout status deployment argocd-server -n argocd
 sudo kubectl rollout status deployment argocd-redis -n argocd
 sudo kubectl rollout status deployment argocd-repo-server -n argocd
@@ -21,7 +20,7 @@ printf "\e[1;33m[wils-APPLICATION] : setup wils application to fetch its config 
 sudo kubectl apply -n argocd -f /p3/p3/confs/argocd_app1.yaml
 
 printf "\e[1;33m[wils-APPLICATION] : make ARGOCD UI accesible \e[0m "
-nohup sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 >> argocdlogs.log 2>&1 & 
+#nohup sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 >> argocdlogs.log 2>&1 & 
 #user: admin
 echo "url : https://localhost:8080"
 echo "user: admin"
