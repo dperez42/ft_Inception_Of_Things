d1eee4639db2b1699e42197497e9d82cf95f6329
Daniel <daniel@Air-de-Daniel.local.tld>
2023-12-14 19:55:56 +0100

    j

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -3,5 +3,8 @@ k3d cluster create zamazzalCluster -p "8888:30080"
 
 kubectl cluster-info
 
+kubectl create namespace argocd
 kubectl create namespace dev
 kubectl get namespace
+
+sudo kubectl apply -n argocd -f ../confs/argo_install.yaml
