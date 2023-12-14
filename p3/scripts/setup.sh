2e1278f0b2b530e88d90aa7c4fc8d670125670c4
Daniel <daniel@Air-de-Daniel.local.tld>
2023-12-14 21:45:33 +0100

    j

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -6,24 +6,23 @@ sudo kubectl cluster-info
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
 sudo kubectl get namespace
-
+printf "\e[1;33m[ARGOCD]🐙 : Installing K3S.\n\e [0m"
 echo "[ARGOCD] : install argocd and wait for pods to rollout..."
 sudo kubectl apply -n argocd -f /p3/p3/confs/argocd_install.yaml
 sudo kubectl apply -n argocd -f /p3/p3/confs/argocd_ingress.yaml
 
-echo "################################################################################"
-echo "[ARGOCD-ROLLOUT] : wait for argocd pods to be running"
+printf "\e[1;33m[ARGOCD]🐙 : wait for argocd pods to be running [0m"
 sudo kubectl rollout status deployment argocd-server -n argocd
 sudo kubectl rollout status deployment argocd-redis -n argocd
 sudo kubectl rollout status deployment argocd-repo-server -n argocd
 sudo kubectl rollout status deployment argocd-dex-server -n argocd
 
-echo "################################################################################"
-echo "[wils-APPLICATION] : setup wils application to fetch its config from our github repo"
+printf "\e[1;33m[wils-APPLICATION] : setup wils application to fetch its config from our github repo [0m "
 sudo kubectl apply -n argocd -f /p3/p3/confs/argocd_app1.yaml
 
-echo "############################################################################"
-echo "############################## ALL SET !! ##################################"
-echo "######################### ARGOCD : localhost:8080 ##########################"
-echo "######################### WIL-APP: localhost:8888 ##########################"
-echo "############################################################################"
+sudo kubectl apply -n argocd -f /p3/p3/confs/argocd_app1.yaml
+printf "\e[1;33m############################################################################"
+printf "############################## ALL SET !! ##################################"
+printf "######################### ARGOCD : localhost:8080 ##########################"
+printf "######################### WIL-APP: localhost:8888 ##########################"
+printf "############################################################################ [0m"
