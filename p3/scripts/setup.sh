5d94f3ae1e458ae737b67f297f1eeed6f5f8bc81
Daniel <daniel@Air-de-Daniel.local.tld>
2023-12-14 21:28:06 +0100

    j

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -8,8 +8,8 @@ sudo kubectl create namespace dev
 sudo kubectl get namespace
 
 echo "[ARGOCD] : install argocd and wait for pods to rollout..."
-sudo kubectl apply -n argocd -f p3/confs/argo_install.yaml
-sudo kubectl apply -n argocd -f p3/confs/argo_ingress.yaml
+sudo kubectl apply -n argocd -f /p3/p3/confs/argo_install.yaml
+sudo kubectl apply -n argocd -f /p3/p3/confs/argo_ingress.yaml
 
 echo "################################################################################"
 echo "[ARGOCD-ROLLOUT] : wait for argocd pods to be running"
@@ -20,7 +20,7 @@ sudo kubectl rollout status deployment argocd-dex-server -n argocd
 
 echo "################################################################################"
 echo "[wils-APPLICATION] : setup wils application to fetch its config from our github repo"
-sudo kubectl apply -n argocd -f p3/confs/argocd_app1.yaml
+sudo kubectl apply -n argocd -f /p3/p3/confs/argocd_app1.yaml
 
 echo "############################################################################"
 echo "############################## ALL SET !! ##################################"
