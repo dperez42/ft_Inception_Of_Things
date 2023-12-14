305e47a85e1f93f178bbc480e67be7e05bfca122
Daniel <daniel@Air-de-Daniel.local.tld>
2023-12-14 21:18:39 +0100

    j

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,11 +1,11 @@
 sudo k3d cluster create p3
-k3d cluster create zamazzalCluster -p "8888:30080"
+#k3d cluster create zamazzalCluster -p "8888:30080"
 
-kubectl cluster-info
+sudo kubectl cluster-info
 
-kubectl create namespace argocd
-kubectl create namespace dev
-kubectl get namespace
+sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
+sudo kubectl get namespace
 
 echo "[ARGOCD] : install argocd and wait for pods to rollout..."
 sudo kubectl apply -n argocd -f ../confs/argo_install.yaml
