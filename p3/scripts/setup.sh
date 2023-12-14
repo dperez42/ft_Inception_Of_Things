6bc66035660596d7a36341075ea6288741df69d2
Daniel <daniel@Air-de-Daniel.local.tld>
2023-12-14 20:10:11 +0100

    j

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -7,4 +7,5 @@ kubectl create namespace argocd
 kubectl create namespace dev
 kubectl get namespace
 
+echo "[ARGOCD] : install argocd and wait for pods to rollout..."
 sudo kubectl apply -n argocd -f ../confs/argo_install.yaml
