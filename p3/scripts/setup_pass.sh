80f50b5f29d910d5b2fa75f1beff2d4e10174b73
Daniel <daniel@Air-de-Daniel.local.tld>
2023-12-19 16:42:24 +0100

    j

--- p3/scripts/setup_pass.sh
+++ p3/scripts/setup_pass.sh
@@ -1,4 +1,5 @@
 sudo argocd account bcrypt --password admin > password.log
+echo $(sudo cat password.log)
 sudo kubectl -n argocd patch secret argocd-secret -p '{"stringData": {"admin.password": "'$(sudo cat password.log)'","admin.passwordMtime": "'$(date +%FT%T%Z)'"}}'
 #sudo rm password.log
 
