0c905460013f3f3c5d9600b2ce05aec8e4c9c4e4
Daniel <daniel@Air-de-Daniel.local.tld>
2023-12-19 16:25:40 +0100

    j

--- p3/scripts/setup_pass.sh
+++ p3/scripts/setup_pass.sh
@@ -14,3 +14,16 @@ printf "######################### ARGOCD : localhost:8080 ######################
 printf "######################### WIL-APP: localhost:8888 ##########################"
 printf "############################################################################ \e[0m"
 printf "\e[1;33m[wils-APPLICATION] : setup wils application to fetch its config from our github repo  \n\e[0m "
+
+printf "\e[0;33m url : https://localhost:8080 \n"
+printf "user: admin"
+echo -n "password: "
+sudo kubectl -n argocd get secret argocd-secret -o jsonpath="{.data.password}" | base64 -d
+echo "\n"
+
+printf "\n\e[0;33m############################################################################"
+printf "############################## ALL SET !! ##################################"
+printf "######################### ARGOCD : localhost:8080 ##########################"
+printf "######################### WIL-APP: localhost:8888 ##########################"
+printf "############################################################################ \e[0m"
+printf "\e[1;33m[wils-APPLICATION] : setup wils application to fetch its config from our github repo  \n\e[0m "
